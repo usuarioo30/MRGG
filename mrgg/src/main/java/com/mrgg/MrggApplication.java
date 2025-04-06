e0ed6d188784a93fbc61512941984435d5e9f112
usuarioo30 <165648159+usuarioo30@users.noreply.github.com>
2025-04-06 21:08:07 +0200

    carpeta backend

---
+++ mrgg/src/main/java/com/mrgg/MrggApplication.java
@@ -0,0 +1,13 @@
+package com.mrgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrggApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrggApplication.class, args);
+	}
+
+}
---
+++ mrgg/src/test/java/com/mrgg/MrggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
